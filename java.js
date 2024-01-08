89610f52d291936f052904b1583d74a36f5d9328
lihazkhan1214 <lehazali123@gmail.com>
2024-01-08 16:06:37 +0500

    add project

---
+++ java.js
@@ -0,0 +1,35 @@
+// let a=2;
+// postfix increament
+// let b=a++;
+
+
+// prefix increament
+// let b=++a;
+// a=a+1;
+
+// console.log(b)
+
+
+// data types
+
+// number 
+// float 
+// let height=5.8;
+// console.log(height)
+// integers
+// let b=14;
+// console.log(b)
+// string
+// let name='ali';
+// let firstname="yasir";
+// let lastname="abbas"
+// let b='2';
+// let d='3';
+
+
+// console.log(firstname+ " "+lastname);
+
+// boolean
+let a=true;
+let b=false;
+console.log(typeof(a))
